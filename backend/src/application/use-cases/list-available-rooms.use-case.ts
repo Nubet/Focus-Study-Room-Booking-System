92b4ffc6456dbac574f71eaa4c8521ef6291c826
Norbert Fila <nubet2137@gmail.com>
2026-04-28 19:28:13 +0200

    feat(green): implement list available rooms use cas

---
+++ backend/src/application/use-cases/list-available-rooms.use-case.ts
@@ -0,0 +1,36 @@
+import { Reservation } from "../../domain/entities/reservation.js";
+
+type Room = {
+  id: string;
+};
+
+type RoomRepository = {
+  findAll: () => Promise<Room[]>;
+};
+
+type ReservationRepository = {
+  findByTimeRange: (startTime: Date, endTime: Date) => Promise<Reservation[]>;
+};
+
+type ListAvailableRoomsInput = {
+  startTime: Date;
+  endTime: Date;
+};
+
+export class ListAvailableRoomsUseCase {
+  constructor(
+    private readonly roomRepository: RoomRepository,
+    private readonly reservationRepository: ReservationRepository
+  ) {}
+
+  async execute(input: ListAvailableRoomsInput): Promise<Room[]> {
+    const [rooms, overlappingReservations] = await Promise.all([
+      this.roomRepository.findAll(),
+      this.reservationRepository.findByTimeRange(input.startTime, input.endTime)
+    ]);
+
+    const reservedRoomIds = new Set(overlappingReservations.map((item) => item.roomId));
+
+    return rooms.filter((room) => !reservedRoomIds.has(room.id));
+  }
+}
