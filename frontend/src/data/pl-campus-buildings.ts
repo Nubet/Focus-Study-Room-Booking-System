169c6f37055481a0b4f26feeff5943e9c9c8768b
Norbert Fila <nubet2137@gmail.com>
2026-04-29 18:55:18 +0200

    feat(backend): add example Campus Buildings

---
+++ frontend/src/data/pl-campus-buildings.ts
@@ -0,0 +1,13 @@
+export type CampusBuilding = {
+  code: string
+  name: string
+}
+
+export const plCampusBuildings: CampusBuilding[] = [
+  { code: 'A1', name: 'Main Building A1' },
+  { code: 'A2', name: 'Main Building A2' },
+  { code: 'B1', name: 'Engineering Building B1' },
+  { code: 'B2', name: 'Engineering Building B2' },
+  { code: 'C1', name: 'Science Building C1' },
+  { code: 'C2', name: 'Science Building C2' }
+]
